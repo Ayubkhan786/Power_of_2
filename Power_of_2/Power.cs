2e575e53f5d91d6144fad7b090ec19875305e628
your Name <mohamedabdulayubm@gmail.com>
2022-07-13 09:34:47 +0530

    [Ayub] The Powers of 2

---
+++ Power_of_2/Power.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power_of_2
+{
+    public class Power
+    {
+        public void FindPower()
+        {
+            int N = 0;  //Intializing the value for N
+            while (N < 31)     //  N value is Given as less than 31
+            {
+                double value =Math.Pow(2, N); //Used Math.pow Method 
+                Console.WriteLine(value);
+                N++;   //Increments the Value after One Iteration 
+            }
+        }
+    }
+}
---
+++ Power_of_2/Program.cs
@@ -0,0 +1,7 @@
+﻿using Power_of_2;
+Console.WriteLine("The Results shows the Power of 2 until 2^31");
+
+Power pow = new Power();
+pow.FindPower();
+
+
